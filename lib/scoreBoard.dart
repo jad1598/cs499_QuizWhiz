a2373391dbe35810489ebe472ed1ef3a8e538f99
Steven Enriquez <stevenenriquez123@gmail.com>
2020-05-02 16:06:13 -0700

    initialized scores to 0 so it doesnt say null

--- lib/scoreBoard.dart
+++ lib/scoreBoard.dart
@@ -2,24 +2,24 @@ import 'package:flutter/material.dart';
 import 'package:quiz_whiz/homePage.dart';
 import 'quiz.dart';
 import 'chooseQuiz.dart';
+
 var count;
-int countC, countH, countE;
+int countC = 0;
+int countH = 0;
+int countE = 0;
+
 class ScoreBoard extends StatelessWidget {
   static String id = "scoreboard";
   @override
   Widget build(BuildContext context) {
     count = getCount();
 
-    if(getType() == 'C')
-    {
+    if (getType() == 'C') {
       countC = count;
-    }
-    else if(getType() =='H')
-    {
+      print(count);
+    } else if (getType() == 'H') {
       countH = count;
-    }
-    else if(getType() =='E')
-    {
+    } else if (getType() == 'E') {
       countE = count;
     }
 
@@ -29,52 +29,46 @@ class ScoreBoard extends StatelessWidget {
         title: Text("Scoreboard"),
       ),
       body: Center(
-        child: ListView(
-            padding: EdgeInsets.all(15),
-            children: <Widget>[
-              Padding(
-                padding: const EdgeInsets.all(15.0),
-                child: Text("Quiz Scores",
-                  style: TextStyle(fontSize: 20),
-                  textAlign: TextAlign.center,
+        child: ListView(padding: EdgeInsets.all(15), children: <Widget>[
+          Padding(
+            padding: const EdgeInsets.all(15.0),
+            child: Text(
+              "Quiz Scores",
+              style: TextStyle(fontSize: 20),
+              textAlign: TextAlign.center,
+            ),
+          ),
+          RaisedButton(
+              textColor: Colors.white,
+              color: Colors.blue,
+              child: Text('Computer Science: ' + countC.toString() + "/5"),
+              onPressed: () {}),
+          RaisedButton(
+              textColor: Colors.white,
+              color: Colors.blue,
+              child: Text('History: ' + countH.toString() + "/5"),
+              onPressed: () {}),
+          RaisedButton(
+              textColor: Colors.white,
+              color: Colors.blue,
+              child: Text('Engineering: ' + countE.toString() + "/5"),
+              onPressed: () {}),
+          FlatButton(
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) {
+                    return HomePage();
+                  },
                 ),
-              ),
-              RaisedButton(
-                  textColor: Colors.white,
-                  color: Colors.blue,
-                  child: Text('Computer Science: '+ countC.toString()+ "/5"),
-                  onPressed: () {
-                  }
-              ),
-              RaisedButton(
-                  textColor: Colors.white,
-                  color: Colors.blue,
-                  child: Text('History: '+ countH.toString()+ "/5"),
-                  onPressed: () {}
-              ),
-              RaisedButton(
-                  textColor: Colors.white,
-                  color: Colors.blue,
-                  child: Text('Engineering: '+ countE.toString()+ "/5"),
-                  onPressed: () {}
-              ),
-              FlatButton(
-                onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) {
-                        return HomePage();
-                      },
-                    ),
-                  );
-                },
-                child: Text(
-                  "Go Home",
-                ),
-              )
-            ]
-        ),
+              );
+            },
+            child: Text(
+              "Go Home",
+            ),
+          )
+        ]),
       ),
     );
   }
